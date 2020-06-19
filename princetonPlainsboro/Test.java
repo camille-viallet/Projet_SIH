b8eba0ccefa47a1b3a875fadf224c4584045581e
Camille VIALLET <camilleviallet73@gmail.com>
2020-06-19 10:00:23 +0200

    Modification tests

--- princetonPlainsboro/Test.java
+++ princetonPlainsboro/Test.java
@@ -1,26 +1,26 @@
 package princetonPlainsboro;
 
-
 class Test {
 
     public static void main(String[] args) {
         LectureXML test = new LectureXML("dossiers.xml");
         DossierMedical dossierMedical = test.getDossier();
-        
+
         Medecin medecin = new Medecin("HOUSE", "Gregory", "123", "heyho", "06.68.26.59.36", Specialite.CARDIOLOGIE);
 
-        Acte a = new Acte(Code.CS, 5, "Medecine générale", medecin, new Date(10,6,2020,15,45), Type.DIAG, "En pleine santé");
-        Patient patient = new Patient("Claire", "DUPRE", "28 Ch.des Vignes 98560 NEUILLY", new Date(25,6,1960,8,45), "192011300135260", 52, 1.82);
-        FicheDeSoins fiche = new FicheDeSoins(patient, medecin, new Date(10,6,2020,15,45), 15);
+        Acte a = new Acte(Code.CS, 5, "Medecine générale", medecin, new Date(10, 6, 2020, 15, 45), Type.DIAG, "En pleine santé");
+        Patient patient = new Patient("Claire", "DUPRE", "28 Ch.des Vignes 98560 NEUILLY", new Date(25, 6, 1960, 8, 45), "192011300135260", 52, 1.82);
+        FicheDeSoins fiche = new FicheDeSoins(patient, medecin, new Date(10, 6, 2020, 15, 45), 15);
         fiche.ajouterActe(a);
         System.out.println(fiche);
-        
-        System.out.println("# TEST SECU - Attendu false : "+Patient.verifierSecu("025896df3548365"));
-        System.out.println("# TEST SECU - Attendu true : "+Patient.verifierSecu("192011300135260"));
-        
-        Date d1 = new Date(10,6,2020,15,45);
-        //System.out.println("# TEST VERIFIER DATE - Attendu true : "+ );
-        
-        
+        System.out.println("");
+
+        System.out.println("# TEST SECU - Attendu false : " + Patient.verifierSecu("025896df3548365"));
+        System.out.println("# TEST SECU - Attendu true : " + Patient.verifierSecu("192011300135260"));
+        System.out.println("");
+
+        System.out.println("# TEST DATE - Attendu false : " + new Date(45, 6, 2020, 15, 45).verifierDate());
+        System.out.println("# TEST DATE - Attendu true : " + new Date(12, 6, 2020, 15, 45).verifierDate());
+
     }
 }
